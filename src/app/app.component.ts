7daaf6a0c42b2dbaca6e3b914b9abfbd148418d2
MariaLisboa1 <zaza.naza95@gmail.com>
2018-08-09 10:13:57 -0300

    Apenas para teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from "./student/student.model";
+import * as schedule from 'node-schedule';
 
 @Component({
   selector: 'jad-root',
@@ -13,4 +14,14 @@ export class AppComponent {
     { name: 'Leia', isJedi: false },
     { name: 'Han Solo', isJedi: false }
   ]
+
+  ngOnInit(){
+    var j = schedule.scheduleJob('0 17 ? * 0,4-6', function(){
+      console.log('Today is recognized by Rebecca Black!');
+    });
+
+    var j = schedule.scheduleJob('0 1 * * *', function(fireDate){
+      console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+    });
+  }
 }
